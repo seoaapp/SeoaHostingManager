51d80cd5a26fb81422d4f5708a2a29017b7a3bf0
PMHStudio <pmhstudio.pmh@gmail.com>
2019-08-03 23:08:57 +0900

    Fixed object Object Error

--- discord/webhook/index.js
+++ discord/webhook/index.js
@@ -12,10 +12,10 @@ switch (process.argv[2]) {
   case 'start':
     const startEmb = new discord.RichEmbed()
       .setColor(0xff7de8).setAuthor('Seoa Hosting', 'https://cdn.discordapp.com/attachments/605052179132186635/607113749534343188/profile.png')
-      .setTitle('Seoa봇 서비스를 복구 했습니다!').setDescription('@here\n이제 Seoa의 기능들을 다시 사용하실 수 있습니다').setFooter('만약 버그를 발견했을경우 @staff에게 알려주세요')
+      .setTitle('Seoa봇 서비스를 복구 했습니다!').setDescription('이제 Seoa의 기능들을 다시 사용하실 수 있습니다').setFooter('만약 버그를 발견했을경우 @staff에게 알려주세요')
       .setThumbnail('https://cdn.discordapp.com/attachments/605052179132186635/607115547519483904/1.png')
     
-    webhook.send(startEmb)
+    webhook.send('@here', startEmb)
     break;
 
   case 'stop':
@@ -34,7 +34,7 @@ switch (process.argv[2]) {
       stopEmbed.setTitle('Seoa봇 서비스가 오류로 인해 잠시 중단되었습니다').setDescription('에러코드: ' + process.argv[3] + ', 스텝분들 빠른 복구 부탁드립니다').setColor(0xff0000)
     }
 
-    webhook.send('<@&605027221094137856>' + stopEmbed)
+    webhook.send('<@&605027221094137856>', stopEmbed)
     break;
 
   default:
